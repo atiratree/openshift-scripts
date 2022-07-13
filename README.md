362920936ea852a46b3f409a5142233b58142329
Filip Křepinský <atiratree@gmail.com>
2022-07-13 17:34:42 +0200

    refactor pods and make watch command replacable with viddy

--- README.md
+++ README.md
@@ -6,14 +6,18 @@
 - oc
 - lnav
 
+### Optional
+
+- viddy
+
 ## Usage
 
 Create aliases for the scripts and put them in your `.bashrc`/`.zshrc`
 
 ```bash
 alias logs="/path/to/watch/logs.sh"
-alias pods="FILTER=Completed /path/to/watch/pods.sh"
-alias podss="/path/to/watch/pods.sh"
+alias pods="WATCH_IMPL=viddy FILTER=Completed /path/to/watch/pods.sh"
+alias podss="WATCH_IMPL=viddy /path/to/watch/pods.sh"
 alias gettoken="/path/to/auth/get-token.sh"
 alias project="/path/to/misc/project.sh"
 ```
--- watch/pods.sh
+++ watch/pods.sh
@@ -7,17 +7,22 @@ NAMESPACE_DIR="$(realpath "${SCRIPTS_DIR}/../namespace")"
 
 NS="$("${NAMESPACE_DIR}"/shortcut-to-ns.sh ${1} )"
 FILTER="${FILTER:-}"
+WATCH_IMPL="${WATCH_IMPL:-watch}" # alternatives: viddy
 
-if [[ -z "$NS" ]]; then
-    if [[ -z "$FILTER" ]]; then
-        watch -n 1  "oc get pods -owide"
-    else
-        watch -n 1  "oc get pods -owide | grep -v ${FILTER}"
-    fi
-else
-    if [[ -z "$FILTER" ]]; then
-        watch -n 1  "oc get pods -owide -n $NS"
-    else
-        watch -n 1  "oc get pods -owide -n $NS | grep -v ${FILTER}"
-    fi
+if ! type "${WATCH_IMPL}" &> /dev/null; then
+    echo "${WATCH_IMPL} command is required"
+    exit 1
 fi
+
+COMMAND="oc get pods -owide"
+
+if [[ -n "${NS}" ]]; then
+    COMMAND="${COMMAND} -n ${NS}"
+fi
+
+if [[ -n "${FILTER}" ]]; then
+    COMMAND="${COMMAND} | grep -v ${FILTER}"
+fi
+
+"${WATCH_IMPL}" -n 1 "${COMMAND}"
+
